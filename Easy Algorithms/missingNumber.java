4a8747747c544341b67498d40a5c2e4d963b462d
KhoaITdn <khoabkdn2003@gmail.com>
2025-05-14 22:46:51 +0700

    up eassy algorithms

---
+++ Easy Algorithms/missingNumber.java
@@ -0,0 +1,27 @@
+// cách 1 dùng hashset để duyệt qua các phân tử rồi sau đó sẽ xem xem phần thử index đó có ở hashset vừa duyêtj hay không
+// nếu không có thì in ra index đó 
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        for (int i = 0; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1; // không xảy ra nếu đầu vào hợp lệ
+    }
+}
+
+
+// cách 2 : dùng công thức sum- sumofnum
+
+
